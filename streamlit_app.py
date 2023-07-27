547331e5ef3fd8ce1cac0391cec2d5286f6ec378
kishoreknot <61914331+kishoreknot@users.noreply.github.com>
2023-07-27 14:31:17 +0530

    Update streamlit_app.py
    Added more item to Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('*************My Parents New Healthy Diner*************')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('HArd-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 HArd-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
