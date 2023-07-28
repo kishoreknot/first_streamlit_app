0e383c67cfbc8aed128f71b81b9b4d3de53d7c91
kishoreknot <61914331+kishoreknot@users.noreply.github.com>
2023-07-29 00:11:00 +0530

    Update streamlit_app.py
    updated index of a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('fruit')
 
-streamlit.multiselect('Please select some fruits:', list(my_fruit_list.fruit))
+streamlit.multiselect('Please select some fruits:', list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
