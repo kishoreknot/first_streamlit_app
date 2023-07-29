08634987997aafcaed3586800d5063517ac9641a
kishoreknot <61914331+kishoreknot@users.noreply.github.com>
2023-07-29 10:28:01 +0530

    Update streamlit_app.py
    Added code to get some data from fruityvice webservice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title('*************My Parents New Healthy Diner*************')
 streamlit.header('Breakfast Favourites')
@@ -18,3 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #to display
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response)
